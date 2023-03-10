439ac59ec7050268d45b3685b0285986cbb351e4
David Stinnette <dastinnette@gmail.com>
2023-03-10 14:52:02 -0500

    Refactored ruby/resistor-color-duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,27 +1,11 @@
 class ResistorColorDuo
     def self.value(colors)
-      color_values = {
-        black: 0,
-        brown: 1,
-        red: 2,
-        orange: 3,
-        yellow: 4,
-        green: 5,
-        blue: 6,
-        violet: 7,
-        grey: 8,
-        white: 9
-      }
-      
-      first_color = colors[0].to_sym
-      second_color = colors[1].to_sym
-  
-      first_color_value = color_values[first_color]
-      second_color_value = color_values[second_color]
-  
-      values = []
-      values.push(first_color_value)
-      values.push(second_color_value)
-      return values.join.to_i
+        color_values = %w[black brown red orange yellow green blue violet grey white]
+
+        values = colors.first(2).map do |color|
+            color_values.index(color)
+        end
+
+        values.join.to_i 
     end 
   end 
