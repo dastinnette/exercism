992518f5ef8133c1219fb34b4c46e73e13e9c65f
David Stinnette <dastinnette@gmail.com>
2023-03-10 10:17:44 -0500

    Refactored ruby/leap

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,9 +1,5 @@
 class Year
     def self.leap?(year)
-        if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
-            true
-        else
-            false
-        end 
+        (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)
     end
 end
