96bbe2ff7397f37e61080531bc2682e9ed6c24b0
David Stinnette <dastinnette@gmail.com>
2024-03-12 11:37:14 -0400

    Refactored python/guidos-lasagna

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -7,12 +7,12 @@ This is a module docstring, used to describe the functionality
 of a module and its functions and/or classes.
 """
 
-
 #TODO: define the 'EXPECTED_BAKE_TIME' constant.
 EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
 
 #TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
-def bake_time_remaining(minutes):
+def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
@@ -23,15 +23,12 @@ def bake_time_remaining(minutes):
     based on the `EXPECTED_BAKE_TIME`.
     """
 
-    return EXPECTED_BAKE_TIME - minutes
-
-
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 #TODO: Define the 'preparation_time_in_minutes()' function below.
 # You might also consider using 'PREPARATION_TIME' here, if you have it defined.
 def preparation_time_in_minutes(layers):
-    return layers * 2
-
+    return layers * PREPARATION_TIME
 
 #TODO: define the 'elapsed_time_in_minutes()' function below.
 # Remember to add a docstring (you can copy and then alter the one from bake_time_remaining.)
