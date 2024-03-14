fe5d6a6d83900fd19c611470c5a8490734715160
David Stinnette <dastinnette@gmail.com>
2024-03-14 16:46:54 -0400

    Refactored python/grains

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,7 +1,7 @@
 def square(number):
-    if not 1 <= integer_number <= 64:
+    if not 1 <= number <= 64:
         raise ValueError('square must be between 1 and 64')
-    return 1 << (integer_number - 1)
+    return 2 ** (number - 1)
 
 def total():
-    return (1 << 64) - 1
+    return (2 ** 64) - 1
