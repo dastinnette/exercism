5e42d52f530b9ff2fb44f35165960a97febdefd5
David Stinnette <dastinnette@gmail.com>
2024-03-25 18:41:09 -0400

    Completed python/leap

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,6 @@
 def leap_year(year):
-    pass
+    if year % 100 == 0 and year % 400 == 0:
+        return True
+    elif year % 4 == 0 and year % 100 != 0:
+        return True 
+    return False 
