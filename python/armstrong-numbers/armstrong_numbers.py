1ed579217fbc94a6e3d4b50a6eb738f8b13b59f0
David Stinnette <dastinnette@gmail.com>
2024-03-25 18:58:23 -0400

    Completed python/armstrong_numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,5 @@
 def is_armstrong_number(number):
-    pass
+    num_str = str(number)
+    num_digits = len(num_str)
+    armstrong_sum = sum(int(digit) ** num_digits for digit in num_str)
+    return armstrong_sum == number
