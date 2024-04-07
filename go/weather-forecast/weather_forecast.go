9dca3778784376e88240173c5ee978ac88aefc78
David Stinnette <dastinnette@gmail.com>
2024-04-07 13:34:46 -0400

    Completed go/weather-forecast

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,12 @@
+// Package weather provides data from the weather.com API.
 package weather
 
+// CurrentCondition represents a certain weather condition.
 var CurrentCondition string
+// CurrentLocation represents a certain geographic location.
 var CurrentLocation string
 
+// Forecast takes a city and condition as strings and returns a string reporting the current weather in a specific location.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
