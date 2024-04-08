a4b5b2f1f0c82b727285a65f1d7f8abac9b8188e
David Stinnette <dastinnette@gmail.com>
2024-04-08 10:22:26 -0400

    Refactored python/collatz

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -3,12 +3,9 @@ def steps(number):
         raise ValueError("Only positive integers are allowed")
 
     count = 0
+    
     while number != 1:
-        if number % 2 == 1:
-            number = (number * 3) + 1
-            count += 1
-        else:
-            number = number / 2
-            count += 1
+        number = 3 * number + 1 if number % 2 else number / 2
+        count += 1
 
     return count
