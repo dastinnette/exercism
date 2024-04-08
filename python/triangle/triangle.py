27b7a3e4472f34d9129b72393a943b5f8580e527
David Stinnette <dastinnette@gmail.com>
2024-04-08 13:47:26 -0400

    Refactored python/triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,26 +1,14 @@
-def is_triangle(sides):
-    if sides[0] > 0 and sides[1] > 0 and sides[2] > 0:
-        if sides[0] + sides[1] >= sides[2] and sides[1] + sides[2] >= sides[0] and sides[0] + sides[2] >= sides[1]:
-            return True
-        else:
-            return False
-
 def equilateral(sides):
-    if is_triangle(sides) and sides[0] == sides[1] == sides[2]:
-        return True
-    else:
-        return False
-
-
+    a, b, c = sides
+    return is_triangle(sides) and (a == b == c)
+        
 def isosceles(sides):
-    if is_triangle(sides) and (sides[0] == sides[1] or sides[0] == sides[2] or sides[1] == sides[2]):
-        return True
-    else:
-        return False
-
-
+    a, b, c = sides
+    return is_triangle(sides) and (a == b or b == c or a == c)
+         
 def scalene(sides):
-    if is_triangle(sides) and sides[0] != sides[1] and sides[0] != sides[2] and sides[1] != sides[2]:
-        return True
-    else:
-        return False
+    return is_triangle(sides) and not isosceles(sides)
+        
+def is_triangle(sides):
+    a, b, c = sides    
+    return (a + b > c) and (b + c > a) and (a + c > b)
