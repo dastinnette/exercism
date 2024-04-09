f20a33af96ee75aa70087bd70b80dac537258b78
David Stinnette <dastinnette@gmail.com>
2024-04-09 18:03:42 -0400

    Completed python/bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,12 @@
-def response(hey_bob):
-    pass
+def response(hey_bob: str) -> str:
+    hey_bob = hey_bob.strip()
+
+    if hey_bob.isupper() and hey_bob.endswith('?'):
+        return "Calm down, I know what I'm doing!"
+    if hey_bob.isupper():
+        return "Whoa, chill out!"
+    if hey_bob.endswith('?'):
+        return "Sure."
+    if not hey_bob:
+        return "Fine. Be that way!"
+    return "Whatever."
