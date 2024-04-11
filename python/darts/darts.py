34bc5d087603e659052ed5a27908bb07603a76d8
David Stinnette <dastinnette@gmail.com>
2024-04-11 11:16:23 -0400

    Solved python/darts

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,12 @@
 def score(x, y):
-    pass
+    x = abs(x)
+    y = abs(y)
+
+    if x**2 + y**2 <= 1:
+        return 10
+    elif x**2 + y**2 <= 25:
+        return 5
+    elif x**2 + y**2 <= 100:
+        return 1
+    else:
+        return 0
