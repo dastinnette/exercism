5d5aa340b46988a357fd257582475486791fe69f
David Stinnette <dastinnette@gmail.com>
2024-04-16 17:06:49 -0400

    Completed python/isbn-verifier

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,24 @@
 def is_valid(isbn):
-    pass
+    numbers = isbn.replace("-", "")
+
+    if "X" in numbers and numbers[-1] != "X":
+        return False
+    
+    list = []
+    for letter in numbers:
+        if letter == "X":
+            list.append(10)
+        elif letter.isnumeric() == False:
+            return False
+        else:
+            list.append(int(letter))
+    
+    if len(list) != 10:
+        return False
+    
+    isbn_check = (list[0] * 10 + list[1] * 9 + list[2] * 8 + list[3] * 7 + list[4] * 6 + list[5] * 5 + list[6] * 4 + list[7] * 3 + list[8] * 2 + list[9] * 1) % 11
+
+    if isbn_check == 0:
+        return True
+    else:
+        return False
