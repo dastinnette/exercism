fb027249e10e03a4b6a83123f2a9cf4706ddb512
David Stinnette <dastinnette@gmail.com>
2024-04-16 17:21:19 -0400

    Refactored python/isbn-verifier

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,24 +1,18 @@
 def is_valid(isbn):
-    numbers = isbn.replace("-", "")
+    total = 0
+    index = 10
 
-    if "X" in numbers and numbers[-1] != "X":
+    for char in isbn:
+        if char == '-':
+            continue
+        if char == 'X' and index == 1:
+            total += 10
+            index -= 1
+            continue
+        if char.isdigit():
+            total += int(char) * index
+            index -= 1
+            continue
         return False
-    
-    list = []
-    for letter in numbers:
-        if letter == "X":
-            list.append(10)
-        elif letter.isnumeric() == False:
-            return False
-        else:
-            list.append(int(letter))
-    
-    if len(list) != 10:
-        return False
-    
-    isbn_check = (list[0] * 10 + list[1] * 9 + list[2] * 8 + list[3] * 7 + list[4] * 6 + list[5] * 5 + list[6] * 4 + list[7] * 3 + list[8] * 2 + list[9] * 1) % 11
 
-    if isbn_check == 0:
-        return True
-    else:
-        return False
+    return index == 0 and total % 11 == 0
