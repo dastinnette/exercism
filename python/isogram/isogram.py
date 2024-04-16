9b4d4ac5794429fb1e77243f2fdda2aac9a6fb7b
David Stinnette <dastinnette@gmail.com>
2024-04-16 14:15:42 -0400

    Solved python/isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,10 @@
-def is_isogram(string):
-    pass
+import re
+
+def is_isogram(sentence):
+    uniform_sentence = re.sub("[^A-Za-z]","",sentence).lower()
+    
+    for char in uniform_sentence:
+        if uniform_sentence.count(char) > 1:
+            return False
+
+    return True
