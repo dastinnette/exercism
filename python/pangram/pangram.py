4200be82e5efbb927dcbe7977c427ea09b954156
David Stinnette <dastinnette@gmail.com>
2024-04-16 11:50:49 -0400

    Completed python/pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,8 @@
 def is_pangram(sentence):
-    pass
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    for char in alphabet:
+        if char not in sentence.lower():
+            return False
+ 
+    return True
