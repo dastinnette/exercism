3eb908233b6059c81577cdd809cd7d338dd59a82
David Stinnette <dastinnette@gmail.com>
2024-04-18 13:33:51 -0400

    Refactored python/pig-latin

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,23 +1,14 @@
-def translate(string):
-    vowel_sound = ['a', 'e', 'i', 'o', 'u', 'xr', 'yt']
-    consonant_sound = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-    consonant_sound_double = ['ch', 'st', 'th', 'qu']
-    consonant_sound_triple = ['sch', 'thr']
-    words = string.split(" ")
-    translated_words = []
-    
-    for text in words:
-        if text == 'rhythm':
-            return "ythmrhay"
-        elif text[0] in vowel_sound or text[:2] in vowel_sound:
-            translated_words.append(text + 'ay')
-        elif text[0] in consonant_sound and text[1:3] == "qu":
-            translated_words.append(text[3:] + text[:3] + 'ay')
-        elif text[:3] in consonant_sound_triple:
-            translated_words.append(text[3:] + text[:3] + 'ay')
-        elif text[:2] in consonant_sound_double:
-            translated_words.append(text[2:] + text[:2] + 'ay')
-        elif text[0] in consonant_sound:
-            translated_words.append(text[1:] + text[0] + 'ay')
+def translate_word(word):
+    while not word[0] in 'aeiou':
+        if word[0] in 'xy' and not word[1] in 'aeiou':
+            break
+        word = word[1:] + word[0]
+        if word[-1] == 'q' and word[0] == 'u':
+            word = word[1:] + 'u'
+
+    return word + 'ay'
+
+def translate(sentence):
+    return ' '.join([translate_word(word) for word in sentence.split()])
     
     return " ".join(translated_words)
