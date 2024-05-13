093c06ab605285de9376eee15425c643f31b1237
David Stinnette <dastinnette@gmail.com>
2024-05-13 19:18:17 -0400

    Solved ruby/isogram

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -5,3 +5,18 @@ Write your code for the 'Isogram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/isogram` directory.
 =end
+
+class Isogram
+    def self.isogram?(word)
+      word = word.downcase
+      letter_count = Hash.new(0)
+      
+      word.each_char do |char|
+        next if char == ' ' || char == '-'
+        return false if letter_count[char] > 0
+        letter_count[char] += 1
+      end
+      
+      return true
+    end
+  end
